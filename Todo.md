fab34c3fcf14443b9ea87e8f72c121a2cb180c40
mohamed ben khedher <ben.khedhermohamed0@gmail.com>
2026-01-15 17:58:30 +0100

    update

---
+++ Todo.md
@@ -0,0 +1,6 @@
+add github project
+add theme swith
+add lang swith
+update dependency of the project
+remove welcol project not working
+update ui of project
--- src/app/_components/sections/Counters.jsx
+++ src/app/_components/sections/Counters.jsx
@@ -9,38 +9,55 @@ const CountersSection = () => {
     CountersBarAnim();
   }, []);
 
+  // Calculate years of experience from a start date
+  const calculateYearsExperience = (startDate) => {
+    if (!startDate) return 0;
+
+    const start = new Date(startDate);
+    const now = new Date();
+
+    let years = now.getFullYear() - start.getFullYear();
+    const monthDiff = now.getMonth() - start.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && now.getDate() < start.getDate())) {
+      years--;
+    }
+
+    return years;
+  };
+
   return (
     <>
-        {/* counters */}
-        <div className="container-fluid">
-
-        {/* row */}
+      {/* counters */}
+      <div className="container-fluid">
         <div className="row p-30-0">
-            
-            {Data.items.map((item, key) => (
-            <div className="col-md-3 col-6" key={`counters-item-${key}`}>
-                {/* couner frame */}
+          {Data.items.map((item, key) => {
+            const value =
+              item.label === "Years Experience"
+                ? calculateYearsExperience(item.startDate)
+                : item.value;
+
+            return (
+              <div className="col-md-3 col-6" key={`counters-item-${key}`}>
+                {/* counter frame */}
                 <div className="art-counter-frame">
-                    {/* counter */}
-                    <div className="art-counter-box">
-                        {/* counter number */}
-                        <span className="art-counter" data-count={item.value}>0</span>
-                        <span className="art-counter-plus">{item.valueAfter}</span>
-                    </div>
-                    {/* counter end */}
-                    {/* title */}
-                    <h6>{item.label}</h6>
+                  <div className="art-counter-box">
+                    <span className="art-counter" data-count={value}>
+                      0
+                    </span>
+                    <span className="art-counter-plus">{item.valueAfter}</span>
+                  </div>
+                  <h6>{item.label}</h6>
                 </div>
-                {/* couner frame end */}
-            </div>
-            ))}
-
+                {/* counter frame end */}
+              </div>
+            );
+          })}
         </div>
-        {/* row end */}
-
-        </div>
-        {/* counters end */}
+      </div>
+      {/* counters end */}
     </>
   );
 };
+
 export default CountersSection;
