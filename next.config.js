702481e3599d03c5202f91680b1cc8f713e99236
mohamed ben khedher <mohamed.benkhedher@mobile-spot.com>
2026-05-14 16:19:22 +0100

    u

--- next.config.js
+++ next.config.js
@@ -1,21 +1,19 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: false,
   images: {
     unoptimized: true,
   },
   assetPrefix: "/Portfolio/",
   basePath: "/Portfolio",
-  output: "export",
   outputFileTracingRoot: __dirname,
   turbopack: {},
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
